987f59da8f792f50f431d0c152983eaaf928a238
DanioleQ <danoiwy1@gmail.com>
2023-10-15 18:29:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function makeUser() {
+  var userData = {
+    login: document.getElementById("nick").value,
+    password: document.getElementById("password").value,
+    email: document.getElementById("email").value,
+  };
+  console.log("Login: " + userData.login);
+  console.log("Password: " + userData.password);
+  console.log("E-mail: " + userData.email);
+  var jsonData = JSON.stringify(userData);
+  localStorage.setItem("userData", jsonData);
+  document.getElementById("nick").value = "";
+  document.getElementById("password").value = "";
+  document.getElementById("email").value = "";
+  window.location.href = "login.html";
+}
